ab9e9ae7f5af5b98943b4d5f8d37963f3aabad9f
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-08-17 21:32:13 +0530

    chore: Capitalize name and NIC in AddEmployeeForm handleChange

--- src/app/user/mycompanies/[id]/employees/clientComponents/coloumnDefinitions.tsx
+++ src/app/user/mycompanies/[id]/employees/clientComponents/coloumnDefinitions.tsx
@@ -2,9 +2,20 @@ import { LocalizationProvider, DatePicker } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import Link from "next/link";
 import dayjs from "dayjs";
-
+import "dayjs/locale/en-gb";
 import { GridColDef } from "@mui/x-data-grid";
 import { Button } from "@mui/material";
+
+// Set dayjs format for consistency
+dayjs.locale("en-gb");
+
+const ddmmyyyy_to_mmddyyyy = (ddmmyyyy: {
+  split: (arg0: string) => [any, any, any];
+}) => {
+  const [dd, mm, yyyy] = ddmmyyyy.split("-");
+  return `${mm}-${dd}-${yyyy}`;
+};
+
 export const columns: GridColDef[] = [
   {
     field: "memberNo",
@@ -31,24 +42,32 @@ export const columns: GridColDef[] = [
     field: "basic",
     headerName: "Basic",
     flex: 1,
+    editable: true,
+    type: "number",
+    align: "left",
+    headerAlign: "left",
   },
   {
     field: "startedAt",
     headerName: "Started At",
     flex: 1,
     editable: true,
+    valueGetter: (params) => {
+      // Ensure the date is formatted correctly for display
+      return params;
+    },
     renderEditCell: (params) => (
       <LocalizationProvider dateAdapter={AdapterDayjs} adapterLocale="en-gb">
         <DatePicker
           label="Started At"
           openTo="year"
           views={["year", "month", "day"]}
-          value={dayjs(params.value)}
+          value={dayjs(ddmmyyyy_to_mmddyyyy(params.value))}
           onChange={(newDate) => {
             params.api.setEditCellValue({
               id: params.id,
               field: params.field,
-              value: newDate?.format("DD-MM-YYYY") as string,
+              value: newDate ? newDate.format("DD-MM-YYYY") : null,
             });
           }}
           slotProps={{
