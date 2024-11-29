3275538439426e5839538ee9d4f9bd5390b29ac7
anush12345 <75567234+anush12345@users.noreply.github.com>
2024-11-29 23:53:56 +0530

    Refactor ABHFillPDF to use a resolved file path for PDF generation and remove obsolete PDF file

--- src/app/api/employees/abh/ABHFillPDF.tsx
+++ src/app/api/employees/abh/ABHFillPDF.tsx
@@ -1,6 +1,7 @@
 import { PDFDocument } from "pdf-lib";
 import { readFileSync } from "fs";
 import { z } from "zod";
+import path from "path";
 
 // Define schema for employee creation
 export const employeeFormSchema = z.object({
@@ -18,7 +19,7 @@ export const employeeFormSchema = z.object({
 export const ABHFillPDF = async (details: any) => {
   const parsedDetails = employeeFormSchema.parse(details);
 
-  const filePath = "/Form new with translations2024_A1.pdf";
+  const filePath = path.resolve(process.cwd(), "public/abh.pdf");
   const pdfBytes = readFileSync(filePath);
 
   const pdfDoc = await PDFDocument.load(pdfBytes.toString("base64"));
